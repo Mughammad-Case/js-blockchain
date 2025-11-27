f16f944695c19467c0b6eb74e5594cbd82a68ade
Mughammad <caseameen44@gmail.com>
2025-11-27 23:34:53 +0200

    add Proof of Work mining function to Block class

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require("crypto-js/sha256");
 
 class Block {
-    constructor(timestamp, data) {
-        this.index = 0;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = "0";
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-    }
+  constructor(timestamp, data) {
+    this.index = 0;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = "0";
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
 
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
-    }
+  calculateHash() {
+    return SHA256(
+      this.index + this.previousHash + this.timestamp + this.data + this.nonce
+    ).toString();
+  }
 
-    mineBlock(difficulty) {
-
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
     }
+    console.log("Block mined: " + this.hash);
+  }
 }
